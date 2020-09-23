6cdbf2890df54285fce71169e5817f932d9dc677
Immanuel Ngari <imanga254@gmail.com>
2020-09-23 14:52:55 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -16,16 +16,23 @@ function ageChecker(age) {
   if (age >= 13 && age <= 19) {
     return "You are a teenager"
   } else if (age <= 12) {
-    return "You are not a teenager"
+    return "You are a kid"
   } else {
     return "You are a grownup"
   }
 }
 
 function ternaryTeenager(age) {
-
+  return age >=13 && age <=19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
+  switch (age) {
+    case 13:
+      console.log("You are a teenager");
+      break;
+    default:
+      
+  }
 
 }
